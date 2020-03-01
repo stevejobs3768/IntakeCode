7ab779869cfed24985ce3f848decb7effe7c1cc0
Ishan Madan <ishan@mistlogic.com>
2020-02-29 16:28:34 -0800

    joystick control

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -22,120 +23,131 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+    private static final String kDefaultAuto = "Default";
+    private static final String kCustomAuto = "My Auto";
+    private String m_autoSelected;
+    private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  private WPI_TalonSRX frontMagazine;
-  private WPI_TalonSRX backMagazine;
-  private WPI_TalonSRX feederMotor;
+    private WPI_TalonSRX frontMagazine;
+    private WPI_TalonSRX backMagazine;
+    private WPI_TalonSRX feederMotor;
 
-  private SpeedControllerGroup magazine;
+    private SpeedControllerGroup magazine;
 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
-  @Override
-  public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
+    private Joystick joystick;
 
-    SmartDashboard.putBoolean("Elevator Synched", true);
+    /**
+     * This function is run when the robot is first started up and should be used
+     * for any initialization code.
+     */
+    @Override
+    public void robotInit() {
+        m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+        m_chooser.addOption("My Auto", kCustomAuto);
+        SmartDashboard.putData("Auto choices", m_chooser);
 
-    SmartDashboard.putNumber("Magazine Elevator", 0);
+        SmartDashboard.putBoolean("Elevator Synced", true);
 
-    SmartDashboard.putNumber("Front Magazine Motor", 0);
-    SmartDashboard.putNumber("Back Magazine Motor", 0);
+        SmartDashboard.putNumber("Magazine Elevator", 0);
 
-    SmartDashboard.putNumber("Feeder Motor", 0);
+        SmartDashboard.putNumber("Front Magazine Motor", 0);
+        SmartDashboard.putNumber("Back Magazine Motor", 0);
 
-    frontMagazine = new WPI_TalonSRX(2);//Arbirtary
-    frontMagazine.setInverted(true);
+        SmartDashboard.putNumber("Feeder Motor", 0);
 
-    backMagazine = new WPI_TalonSRX(3); //Arbritary
+        frontMagazine = new WPI_TalonSRX(2);// Arbirtary
+        frontMagazine.setInverted(true);
 
-    feederMotor = new WPI_TalonSRX(1);
-    feederMotor.setInverted(true);
+        backMagazine = new WPI_TalonSRX(3); // Arbritary
 
-    //Speed Controllers
-    magazine = new SpeedControllerGroup(frontMagazine, backMagazine);
-  }
+        feederMotor = new WPI_TalonSRX(1);
+        feederMotor.setInverted(true);
 
+        // Speed Controllers
+        magazine = new SpeedControllerGroup(frontMagazine, backMagazine);
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-  }
-
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
-  }
-
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
-  }
-
-  /**
-   * This function is called periodically during operator control.
-   */
-  @Override
-  public void teleopPeriodic() {
-
-
-    if (SmartDashboard.getBoolean("Elevator Synched", true)){
-      magazine.set(SmartDashboard.getNumber("Magazine Elevator", 0));
+        joystick = new Joystick(0);
     }
 
-    else{
-      frontMagazine.set(SmartDashboard.getNumber("Front Magazine Motor", 0));
-      backMagazine.set(SmartDashboard.getNumber("Back Magazine Motor", 0));
+    /**
+     * This function is called every robot packet, no matter the mode. Use this for
+     * items like diagnostics that you want ran during disabled, autonomous,
+     * teleoperated and test.
+     *
+     * <p>
+     * This runs after the mode specific periodic functions, but before LiveWindow
+     * and SmartDashboard integrated updating.
+     */
+    @Override
+    public void robotPeriodic() {
     }
-    
 
-    feederMotor.set(SmartDashboard.getNumber("Feeder Motor", 0));
+    /**
+     * This autonomous (along with the chooser code above) shows how to select
+     * between different autonomous modes using the dashboard. The sendable chooser
+     * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+     * remove all of the chooser code and uncomment the getString line to get the
+     * auto name from the text box below the Gyro
+     *
+     * <p>
+     * You can add additional auto modes by adding additional comparisons to the
+     * switch structure below with additional strings. If using the SendableChooser
+     * make sure to add them to the chooser code above as well.
+     */
+    @Override
+    public void autonomousInit() {
+        m_autoSelected = m_chooser.getSelected();
+        // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+        System.out.println("Auto selected: " + m_autoSelected);
+    }
 
-  }
+    /**
+     * This function is called periodically during autonomous.
+     */
+    @Override
+    public void autonomousPeriodic() {
+        switch (m_autoSelected) {
+            case kCustomAuto:
+                // Put custom auto code here
+                break;
+            case kDefaultAuto:
+            default:
+                // Put default auto code here
+                break;
+        }
+    }
 
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
-  public void testPeriodic() {
-  }
+    /**
+     * This function is called periodically during operator control.
+     */
+    @Override
+    public void teleopPeriodic() {
+        if (joystick.getTrigger()) {
+            if (SmartDashboard.getBoolean("Elevator Synced", true)) {
+                magazine.set(SmartDashboard.getNumber("Magazine Elevator", 0));
+            } else {
+                frontMagazine.set(SmartDashboard.getNumber("Front Magazine Motor", 0));
+                backMagazine.set(SmartDashboard.getNumber("Back Magazine Motor", 0));
+            }
+            feederMotor.set(SmartDashboard.getNumber("Feeder Motor", 0));
+        } else if (joystick.getRawButton(2)) {
+            if (SmartDashboard.getBoolean("Elevator Synced", true)) {
+                magazine.set(-1 * SmartDashboard.getNumber("Magazine Elevator", 0));
+            } else {
+                frontMagazine.set(-1 * SmartDashboard.getNumber("Front Magazine Motor", 0));
+                backMagazine.set(-1 * SmartDashboard.getNumber("Back Magazine Motor", 0));
+            }
+            feederMotor.set(-1 * SmartDashboard.getNumber("Feeder Motor", 0));
+        } else {
+            magazine.set(0);
+            feederMotor.set(0);
+        }
+    }
+
+    /**
+     * This function is called periodically during test mode.
+     */
+    @Override
+    public void testPeriodic() {
+    }
 }
